f5ce1c980b708051b25a5ecdae883ffbf4af8490
Thomas <tjb47.dev@gmail.com>
2023-04-03 20:21:31 -0400

    Initial dist subtree commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
+
 module.exports = () => {
   return {
     mode: 'development',
